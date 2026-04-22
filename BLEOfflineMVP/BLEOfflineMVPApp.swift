bca18a1e480211201ce332aa5fbd62942ce1cdff
MD Aminuzzaman <rminc0102@gmail.com>
2026-04-23 01:39:14 +0600

    Project setup

---
+++ BLEOfflineMVP/BLEOfflineMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEOfflineMVPApp.swift
+//  BLEOfflineMVP
+//
+//  Created by MD Aminuzzaman on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BLEOfflineMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
